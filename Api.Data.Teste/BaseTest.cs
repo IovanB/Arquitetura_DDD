802aaedc006b9db76b550f5a1b28c159d20ed37e
Iovan Barros <iovanbarros@outlook.com>
2021-05-18 15:34:42 -0300

    savings

---
+++ Api.Data.Teste/BaseTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Api.Data.Teste
+{
+    public abstract class BaseTest
+    {
+        public BaseTest()
+        {
+                
+        }
+    }
+
+    public class DbTeste : IDisposable
+    {
+        //private string dataBaseName = $"dbApiTest_"
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
